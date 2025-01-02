7c8d6c2a94f559784671e6a3a66dce329e20c515
Mykola <Pecheryag@gmail.com>
2025-01-02 08:45:55 +0200

    gallery Likes saved

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Image from 'next/image'
 import styles from './Gallery.module.css'
 
@@ -12,6 +12,21 @@ const Gallery: React.FC<GalleryProps> = ({ images }) => {
   const [currentImageIndex, setCurrentImageIndex] = useState(0)
   const [isFullscreen, setIsFullscreen] = useState(false)
   const [likes, setLikes] = useState(0)
+
+  // Отримуємо лайки з локальної пам'яті при завантаженні компонента
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const savedLikes = localStorage.getItem('likes')
+      setLikes(savedLikes ? parseInt(savedLikes, 10) : 0)
+    }
+  }, [])
+
+  // Записуємо лайки у локальну пам'ять при зміні стану
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      localStorage.setItem('likes', likes.toString())
+    }
+  }, [likes])
 
   const handlePrev = () => {
     setCurrentImageIndex((prevIndex) =>
